96205e37c86aa2a0c4273a8d0ebf560c08f3cfe9
Aquinas22 <noah.p.roe@gmail.com>
2026-01-09 00:44:46 -0600

    Add elephant mario design 4.0

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const mario = document.getElementById("mario");
-while true do {
-        const sound = new Audio("sounds/mariocalling.mp3");
-}
+
+const sound = new Audio("sounds/mariocalling.mp3");
+
 if (mario) {
-    const sound = new Audio("sounds/mariocalling.mp3");
 
     mario.addEventListener("click", () => {
         sound.currentTime = 0;
@@ -12,4 +11,7 @@ if (mario) {
         mario.classList.add("bounce");
         setTimeout(() => mario.classList.remove("bounce"), 200);
     });
+}
+else {
+    sound.play();
 }
